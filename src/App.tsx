538c074c2e46ddd87bc5b48ca5eb702f12f6bd25
Igor Nicoletti <igornicoletti@outlook.com>
2024-06-06 18:34:17 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { UseParams } from './hooks/useParams'
+
+export const App = () => {
+  const { data, setFilter } = UseParams()
+
+  if (!data) return
+
+  return (
+    <div className={'relative h-full min-h-screen w-full grid content-center'}>
+      <div className={'w-full max-w-screen-xl mx-auto px-8'}>
+        <div className={'flex items-center justify-center gap-8 py-16'}>
+          {data.map((data) => (
+            <button key={data.id} onClick={() => setFilter(data.id)}>{data.name}</button>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
